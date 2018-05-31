0352f2f9d2a2a95c13c022787e683c590d7f50b2
1757352023@qq.com <1757352023@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2018-05-31 02:52:40 +0000

    测试004
    git-svn-id: svn://svn3.apicloud.com/A6058441380446/%E5%A4%A9%E7%89%9B%E7%BD%91APP%E7%BD%91%E7%82%B9%E7%AB%AF@132 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- html/my/earnintegral/js/earnintegral.js
+++ html/my/earnintegral/js/earnintegral.js
@@ -43,38 +43,13 @@ var vm = new Vue({
               userPhone:vm.websiteInfo
             });
         },
-        //分享注册链接到朋友圈
-        shareWx: function() {
-            wx.isInstalled(function(ret, err) {
-              //检测是否安装了微信客户端
-                if (ret.installed) {
-                    wx.shareWebpage({
-                        apiKey: '',
-                        scene: 'timeline',
-                        title: '天牛网注册链接',
-                        description: '推广注册链接',
-                        thumb: 'widget://image/logo.jpg',
-                        contentUrl: "http://120.26.161.225:8082/tianniu-web/users/userRegister?iPhone=" + vm.websiteInfo
-                    }, function(ret, err) {
-                        if (ret.status) {
-                            toast('分享成功');
-                        } else {
-                            toast('分享失败');
-                        }
-                    });
-                } else {
-                    toast("当前设备未安装微信客户端");
-                }
-            });
-        },
+
         //分享有推广二维码的图片到朋友圈
         shareWxFriends: function() {
-
           var makeSharePic = api.require('makeSharePic');
-          console.log(makeSharePic)
           makeSharePic.makePicture({
               imgUrl: 'http://pic.jj20.com/up/allimg/911/021616153629/160216153629-1.jpg',
-              qrCode: 'http://120.26.161.225:8082/tianniu-web/users/userRegister?iPhone=' + vm.websiteInfo,
+              qrCode: 'http://www.tianniu.net.cn:8082/users/userRegister?iPhone=' + vm.websiteInfo,
               savePath:'cache://image',
               fileName:'myErWeiMa.png',
               // expressType: '活动',
